6d456a11ea0fd13637365bce0927aa1a509661c8
drone117 <98345857+drone117@users.noreply.github.com>
2023-10-02 19:18:37 +0300

    Added project files

---
+++ Source/Test_ART3D/Test_ART3D.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Test_ART3D.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Test_ART3D, "Test_ART3D" );
